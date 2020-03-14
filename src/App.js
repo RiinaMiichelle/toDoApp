4f8a333d10a46a4bddf812c4cc70556b445c56ad
RiinaMiichelle <sarina.m.colosimo@gmail.com>
2020-03-14 15:27:59 -0500

    finished todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { object } from 'prop-types';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      isClicked: false
+      isClicked: false,
+      todoInput: " ",
+      todos: []
     }
   }
 
-  handleButtonClick = () => {
+  upDateTodo = (e) => {
     this.setState ({
-      isClicked: true
+      todoInput: e.target.value
+    })
+  }
+
+  onClickDelete = (index) => {
+    var newTodos = [...this.state.todos]
+    newTodos.splice(index, 1)
+    this.setState({ todos: newTodos })
+  }
+
+  handleButtonClick = () => {
+    var newTodos = [...this.state.todos]
+    newTodos.push(this.state.todoInput);
+    console.log(newTodos, this.state.todos)
+    this.setState ({
+      isClicked: true,
+      todos: newTodos,
+      todoInput: " "
     });
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleButtonClick}></button>
+        <input value={this.state.todoInput} onChange={this.upDateTodo}></input>
+        <button onClick={this.handleButtonClick}>Submit ToDo</button>
+        <ul>
+          {this.state.todos.map((todos, index) => {
+            return (
+            <li key={index}>{todos}
+              <button onClick={()=>{
+                this.onClickDelete(index)
+              }}>Delete ToDo</button>
+            </li>
+            )
+          })}
+        </ul>
       </div>
     );
   }
